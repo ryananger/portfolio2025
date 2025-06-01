882177407a0c071d278b36cf2beab6354619d407
ryananger <anger.ryan.s@gmail.com>
2025-06-01 14:58:06 -0400

    mobile landscape style update

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -45,7 +45,7 @@ const App = function() {
     return (
       <p className={`aboutMe v ${visible ? 'visible' : 'hidden'}`}>
         Welcome! I'm Ry, a full-stack software engineer, and this is my portfolio. 
-        Projects here were developed with React, React Native, Node.js, MongoDB, and hosted on AWS.
+        These projects were developed with React, React Native, Node.js, MongoDB, and hosted on AWS.
         I also have experience with Vue and Angular, SQL, some Python, and I've worked with a number of API's for different purposes, 
         including AI integration, market data retrieval, authentication, and sales.
         <br/><br/>
